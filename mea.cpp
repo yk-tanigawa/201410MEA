c9b6aacd4dad7df510819c02a4debddf37e1b8a6
yk-tanigawa <yk-tanigawa@users.noreply.github.com>
2014-10-24 23:00:58 +0900

    forward backwardに着手

--- mea.cpp
+++ mea.cpp
@@ -10,6 +10,8 @@ using namespace std;
 class sequence;
 class mea;
 
+typedef enum { M, X, Y, } tbltype_t;
+
 class sequence{
   string header; 
   int length;
@@ -19,19 +21,43 @@ class sequence{
 public:
   void set(string, string);
   void dump(int);
+  int get_ary(int i){ return ary.at(i); }
+  int len(){ return length; }
 };
   
 class mea{
   vector<sequence> seq;
-  long double T;
-  long double d, e;
+  long double T, d, e;
   int alph_size;
-  vector<vector <long double> > s; /* substitution matrix */
+  vector<vector <long double> > s;    /* substitution matrix */
   vector<vector<vector <long double> > > forward, backward;
+  long double lpd, lpe;
+  vector<vector <long double> > lpij;
+  long double  lz;
   void read_data_seqs(ifstream &);
   void read_data_params(ifstream &);
+  void forward_backward_init(int, int);
+  void forward_calc(int, int);
+  void backward_calc(int, int);
+  void forward_backward_terminate(int, int);
 public:
+  void set_f(tbltype_t type, int i, int j, long double val){
+    forward.at((int) type).at(i).at(j) = val;
+  }
+  void set_b(tbltype_t type, int i, int j, long double val){
+    backward.at((int) type).at(i).at(j) = val;
+  }
+  long double get_f(tbltype_t type, int i, int j){
+    return forward.at((int) type).at(i).at(j);
+  }
+  long double get_b(tbltype_t type, int i, int j){
+    return backward.at((int) type).at(i).at(j);
+  }
+  long double get_lpij(int i, int j){
+    return lpij.at(seq.at(0).get_ary(i)).at(seq.at(1).get_ary(j));
+  }
   void read_data(char *, char *);
+  void forward_backward();
   void dump(int);
 };
 
@@ -54,6 +80,115 @@ istream &getline_wocomment(char c, istream &is, string &str){
   if(comment_start > 0){ str.erase(comment_start); }
   return is;
 }
+
+inline void mea::forward_backward_init(int n, int m){
+  /* forward variables と backward variablesのメモリを確保 */
+  forward.resize(3); backward.resize(3);
+  for(int t = 0; t < 3; t++){
+    forward.at(t).resize(n);  backward.at(t).resize(n);
+    for(int i = 0; i < n; i++){
+      forward.at(t).at(i).resize(m, 0);
+      backward.at(t).at(i).resize(m, 0);
+    }
+  }
+
+  /* forward変数の初期化 */
+  for(int i = 0; i < n; i++){
+    set_f(M, i, 0, logl(0));
+    set_f(X, i, 0, lpd + (i - 1) * lpe);
+    set_f(Y, i, 0, logl(0)); 
+  }
+  for(int j = 0; j < m; j++){
+    set_f(M, 0, j, logl(0));
+    set_f(X, 0, j, logl(0));
+    set_f(Y, 0, j, lpd + (j - 1) * lpe); 
+  }
+  set_f(M, 0, 0, logl(1)); 
+  set_f(X, 0, 0, logl(0)); 
+  set_f(Y, 0, 0, logl(0));
+
+  /* backward変数の初期化 */  
+  for(int i = 0; i < n - 1; i++){
+    set_b(M, i, m - 1, lpd + (m - i - 1) * lpe);
+    set_b(X, i, m - 1, (m - i) * lpe);
+    set_b(Y, i, m - 1, logl(0)); 
+  }
+  for(int j = 0; j < m - 1; j++){
+    set_b(M, n - 1, j, lpd + (n - j - 1) * lpe);
+    set_b(X, n - 1, j, logl(0));
+    set_b(Y, n - 1, j, (n - j) * lpe); 
+  }
+  set_b(M, n - 1, m - 1, logl(1)); 
+  set_b(X, n - 1, m - 1, logl(0));
+  set_b(Y, n - 1, m - 1, logl(0));
+
+  return;
+}
+
+inline void mea::forward_calc(int n, int m){
+  for(int i = 1; i < n; i++){
+    for(int j = 1; j < m; j++){
+      set_f(M, i, j, 
+	    logl(expl( get_f(M, i - 1, j - 1) ) +
+		 expl( get_f(X, i - 1, j - 1) ) +
+		 expl( get_f(Y, i - 1, j - 1) )) +
+	    get_lpij(i, j));
+      set_f(X, i, j, 
+	    logl(expl( get_f(M, i - 1, j) + lpd ) +
+		 expl( get_f(X, i - 1, j) + lpe )));
+      set_f(Y, i, j, 
+	    logl(expl( get_f(M, i, j - 1) + lpd ) +
+		 expl( get_f(Y, i, j - 1) + lpe )));
+    }
+  }
+  return;
+}
+
+inline void mea::backward_calc(int n, int m){
+  for(int i = n - 2; i >= 0; i--){
+    for(int j = m - 2; j >= 0; j--){
+      set_b(M, i, j, 
+	    logl(expl(get_b(M, i + 1, j + 1) 
+		      + get_lpij(i + 1, j + 1)) +
+		 expl(get_b(X, i + 1, j) + lpd) +
+		 expl(get_b(Y, i, j + 1) + lpd)));
+      set_b(X, i, j, 
+	    logl(expl(get_b(M, i + 1, j + 1) 
+		      + get_lpij(i + 1, j + 1)) +
+		 expl(get_b(X, i + 1, j) + lpe)));
+      set_b(Y, i, j, 
+	    logl(expl(get_b(M, i + 1, j + 1) 
+		      + get_lpij(i + 1, j + 1)) +
+		 expl(get_b(Y, i, j + 1) + lpe)));
+    }
+  }
+  return;
+}
+
+inline void mea::forward_backward_terminate(int n, int m){
+  lz =  logl(expl(get_f(M, n - 1, m - 1)) +
+	     expl(get_f(X, n - 1, m - 1)) + 
+	     expl(get_f(Y, n - 1, m - 1)));
+  cout << lz << endl;
+  cout << expl(lz) << endl;
+  long double temp 
+    = logl(expl(get_b(M, 0, 0) + get_lpij(0, 0)) + 
+	   expl(get_b(X, 0, 0) + lpd) +
+	   expl(get_b(Y, 0, 0) + lpd));
+  cout << temp << endl;
+  return;
+}
+
+inline void mea::forward_backward(){
+  if( seq.size() < 2){ return; }
+  int n = seq.at(0).len(), m = seq.at(1).len();
+  forward_backward_init(n, m);
+  forward_calc(n, m);
+  backward_calc(n, m);
+  forward_backward_terminate(n, m);
+  return;
+}
+
 
 inline vector<string> split(const string &str, char delim){
   /* strを delim でsplitして vector<string> として返す */
@@ -120,6 +255,16 @@ void mea::read_data_params(ifstream &fs){
       sscanf(input_str.at(j).c_str(), "%Lf", &(s.at(i).at(j)));
     }
   }
+
+  /* logをとったパラメータの初期化 */
+  lpij.resize(alph_size);
+  for(int i = 0; i < alph_size; i++){
+    lpij.at(i).resize(alph_size, 0);
+    for(int j = 0; j < alph_size; j++){
+      lpij.at(i).at(j) = s.at(i).at(j) / T;
+    }
+  }
+  lpd = -1.0 * d/T;  lpe = -1.0 * e/T;
   return;
 }
 
@@ -166,5 +311,6 @@ int main(){
   mea job;
   job.read_data((char *)"params.txt", (char *)"sequences.txt");
   job.dump(60);
+  job.forward_backward();
   return 0;
 }
